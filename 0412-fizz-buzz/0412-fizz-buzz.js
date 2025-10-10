af358f8889e4fd58c35294c313bfe214db1d14a2
Mahmoud Abo-Zied <116457362+mahmoudabozied4@users.noreply.github.com>
2025-10-10 21:13:59 +0300

    0412-FIZZ-BUZZ Stats: Time: 1 ms (63.57%), Space: 57.7 MB (66.69%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+  let ans = new Array(n);
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      ans[i -1 ] = `FizzBuzz`;
+    } else if (i % 3 === 0) {
+      ans[i - 1] = `Fizz`;
+    } else if (i % 5 === 0) {
+      ans[i - 1] = `Buzz`;
+    } else {
+      ans[i - 1] = String(i);
+    }
+  }
+  return ans;
+};
