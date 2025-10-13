d45467d82557f5cd4811a10a9a2fbfa6b53e539f
Mahmoud Abo-Zied <116457362+mahmoudabozied4@users.noreply.github.com>
2025-10-13 05:02:24 +0300

    0020-VALID-PARENTHESES Stats: Time: 0 ms (100%), Space: 4.3 MB (55.34%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.go
@@ -0,0 +1,36 @@
+
+func isValid(s string) bool {
+	stack := []rune{}
+
+	for _, char := range s {
+		switch char {
+		case '(', '{', '[':
+			stack = append(stack, char)
+
+		case ')':
+			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+
+		case '}':
+			if len(stack) == 0 || stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+
+		case ']':
+			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+
+		default:
+			return false
+		}
+	}
+	if len(stack) == 0 {
+		return true
+	}
+	return false
+}
