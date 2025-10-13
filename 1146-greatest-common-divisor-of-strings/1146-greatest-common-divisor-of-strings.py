24cf3e6f40a243b7f984698e205304e7fcf91034
Mahmoud Abo-Zied <116457362+mahmoudabozied4@users.noreply.github.com>
2025-10-13 21:46:55 +0300

    1146-GREATEST-COMMON-DIVISOR-OF-STRINGS Stats: Time: 0 ms (100%), Space: 17.8 MB (46.32%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def gcd(self , a , b):
+        if b != 0:
+            return self.gcd(b , a % b)
+        return a
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1+str2 != str2 + str1: return ""
+        g= self.gcd(len(str1) , len(str2))
+        return str1[:g]
