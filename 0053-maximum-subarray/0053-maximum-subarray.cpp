b9fdb39ae77ae50c2f9c6193034ded27b3cd14b7
Mahmoud Abo-Zied <116457362+mahmoudabozied4@users.noreply.github.com>
2025-10-20 04:06:40 +0300

    0053-MAXIMUM-SUBARRAY Stats: Time: 0 ms (100%), Space: 71.9 MB (5.43%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int curSum = 0;
+
+        for (int n : nums) {
+            curSum = max(curSum, 0);
+            curSum += n;
+            maxSum = max(maxSum, curSum);
+        }
+        return maxSum;
+    }
+};
